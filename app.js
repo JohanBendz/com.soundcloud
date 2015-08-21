4c37e7b45d3a4730c12b301de15c4e44cc5b42d6
Emile Nijssen <emilenijssen@gmail.com>
2015-08-21 19:34:56 +0200

    fix

--- app.js
+++ app.js
@@ -1,73 +1,64 @@
 "use strict";
 
 var SC = require('node-soundcloud');
-  
-function App() 
-{
-	
-}
 
-module.exports = App;
+var self = module.exports = {
 
-App.prototype.init = function(){
-	
-	var config = require('./config.json');
- 
-	// Initialize client
-	SC.init({
-		id		: config.client_id,
-		secret	: config.client_secret,
-		uri		: config.redirect_uri
-	});
-	
-	Homey.manager('media').on('search', function( query, callback ){
-	
-		SC.get('/tracks', { q: query }, function(err, tracks) {
+	init: function(){
+		 
+		// Initialize client
+		SC.init({
+			id		: Homey.env.client_id,
+			secret	: Homey.env.client_secret,
+			uri		: Homey.env.redirect_uri
+		});
+		
+		Homey.manager('media').on('search', function( query, callback ){
+		
+			SC.get('/tracks', { q: query }, function(err, tracks) {
+				
+				var result = [];
+				
+				tracks.forEach(function(track){
+					result.push({
+						type		: 'track',
+						id			: track.id,
+						title		: track.title,
+						artist		: track.user.username,
+						album		: false,
+						duration	: track.duration,
+						artwork		: track.artwork_url
+					});
+				});
+				
+				callback( result );
+				
+			});
 			
-			var result = [];
+		})
+		
+		Homey.manager('media').on('play', function( track_id, callback ){
 			
-			tracks.forEach(function(track){
-				result.push({
-					type		: 'track',
+			Homey.log('play', track_id)
+			
+			if( !track_id ) return;
+			
+			SC.get('/tracks/' + track_id, function(err, track) {
+				
+				Homey.manager('media').setTrack({
 					id			: track.id,
 					title		: track.title,
 					artist		: track.user.username,
 					album		: false,
 					duration	: track.duration,
-					artwork		: track.artwork_url
+					artwork		: track.artwork_url,
+					stream_url	: track.stream_url + '?client_id=' + Homey.env.client_id
 				});
+				
 			});
 			
-			callback( result );
-			
 		});
-		
-	})
 	
-	Homey.manager('media').on('play', function( track_id, callback ){
-		
-		if( !track_id ) return;
-		
-		SC.get('/tracks/' + track_id, function(err, track) {
-			
-			Homey.manager('media').setTrack({
-				id			: track.id,
-				title		: track.title,
-				artist		: track.user.username,
-				album		: false,
-				duration	: track.duration,
-				artwork		: track.artwork_url
-			});
-			
-			Homey.log(track)
-			
-			callback();
-			
-		});
-		
-		/*
-		Homey.log( 'play', track_id )
-		*/
-	});
+	}
 	
 }
